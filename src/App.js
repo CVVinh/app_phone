41dafc3c6225f02fcf5e5b85e3782e7a8ec33da9
CVVinh <caovanvinh111@gmail.com>
2023-11-06 17:43:42 +0700

    udpate 6-11-22

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
-// import logo from './logo.svg';
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar";
-import ProductList from "./components/ProductList";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 function App() {
   return (
     <React.Fragment>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<ProductList />} />
-
-        {/* <Route path="*" element={<NotFound />} /> */}
-      </Routes>
+      <Switch></Switch>
     </React.Fragment>
   );
 }
